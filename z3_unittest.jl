254b0f88f851507725c40979481761aa4800117b
Emiko Soroka <emiko@me.com>
2023-04-02 18:11:37 -0700

    starting unittesting

---
+++ z3_unittest.jl
@@ -0,0 +1,10 @@
+using Test, LinearAlgebra
+include("z3_utility.jl")
+
+# https://docs.julialang.org/en/v1/stdlib/Test/
+# Example of a unittest
+# @test π ≈ 3.14 atol=0.01
+
+# BASIC FUNCTIONALITY (TODO)
+
+# Problem solving (TODO)
